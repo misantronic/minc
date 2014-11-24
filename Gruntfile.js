de844efb958ac3461918c56b2a8b4ef87a2797cc
misantronic <david.skx@posteo.de>
2014-11-24 20:36:07 +0100

    removed dalek

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,31 +10,15 @@ module.exports = function(grunt) {
 
 			projects: {
 				files: {
-					'minc.min.js'	: ['minc.js']
+					'src/minc.min.js'	: ['src/minc.js']
 				}
-			}
-		},
-
-		dalek: {
-			options: {
-				"browser": ["phantomjs"],
-				"browsers": [{
-					"chrome": {
-						"port": 5555
-					}
-				}]
-			},
-			dist: {
-				src: ['test/tests.js']
 			}
 		}
 	});
 
 	// load tasks
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-dalek');
 
 	// tasks
-	grunt.registerTask('test', ['dalek']);
-	grunt.registerTask('default', ['dalek', 'uglify']);
+	grunt.registerTask('default', ['uglify']);
 };
