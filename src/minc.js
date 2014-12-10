eeccd1111062c7b6ad2a8e9fb00645e339705abb
misantronic <david.skx@posteo.de>
2014-12-10 18:40:47 +0100

    prevent error on fetching css-file

--- src/minc.js
+++ src/minc.js
@@ -29,68 +29,66 @@
 				}
 			});
 
-			try {
-				!function l(s, d, o, t, I) {
-					j = 0;
-					// AMD define()
-					(w.define =
-						/**
-						 *
-						 * @param {Function|String|Array} x Module function or moduleName or dependencies
-						 * @param {Function|Array} y Module function or dependencies
-						 * @param {Function|undefined} z Module function or undefined
-						 */
-						function(x, y, z) {
-							j = 1;
-							// check multi-use of parameters
-							x.call ? (z = x, x = "", y = []) : x.pop ? (z = y, y = x, x = "") : y.call && (z = y, y = []);
+			!function l(s, d, o, t, I) {
+				j = 0;
+				// AMD define()
+				(w.define =
+					/**
+					 *
+					 * @param {Function|String|Array} x Module function or moduleName or dependencies
+					 * @param {Function|Array} y Module function or dependencies
+					 * @param {Function|undefined} z Module function or undefined
+					 */
+					function(x, y, z) {
+						j = 1;
+						// check multi-use of parameters
+						x.call ? (z = x, x = "", y = []) : x.pop ? (z = y, y = x, x = "") : y.call && (z = y, y = []);
 
-							// save module
-							m.push({ i: x, d: y, m: z });
+						// save module
+						m.push({ i: x, d: y, m: z });
 
-							if(L=y.length)
-								for(d=L, o = []; d--;) { 		// parse dependencies
-									for(g=m.length; g--;) 		// parse modules
-										if(m[g] && y[d] == m[g].i)
-											o.push(m[g].m());
+						if(L=y.length)
+							for(d=L, o = []; d--;) { 		// parse dependencies
+								for(g=m.length; g--;) 		// parse modules
+									if(m[g] && y[d] == m[g].i)
+										o.push(m[g].m());
 
-									L != o.length && 			// validate dependencies
-									C && C.log("Missing", y)
+								L != o.length && 			// validate dependencies
+								C && C.log("Missing", y)
 
-									M[i] = z.apply(w, o);		// assign module callback
-								}
-							else 								// assign module callback
-								M[i] = z()
-						}
-					).amd = 1;
-
-					// load
-					with(document)
-						(d=createElement(t = s.substr(-4) == '.css' ? "link" : "script"))[t == "link" ? "href" : "src"] = s.replace(/https*:/, ""),
-						t == "link" ?
-
-						// load CSS
-						(I = setInterval(function(q) {
-							if(d.sheet && d.sheet.cssRules && d.sheet.cssRules[0] || d[q="styleSheet"] && d[q].rules && d[q].rules[0]) {
-								a[++i] ? l(a[i]) : r && r.apply(_, M);
-								clearInterval(I)
+								M[i] = z.apply(w, o);		// assign module callback
 							}
-						}, 5)) && (d.rel = "stylesheet")
+						else 								// assign module callback
+							M[i] = z()
+					}
+				).amd = 1;
 
-						:
+				// load
+				with(document)
+					(d=createElement(t = s.substr(-4) == '.css' ? "link" : "script"))[t == "link" ? "href" : "src"] = s.replace(/https*:/, ""),
+					t == "link" ?
 
-						// load JS
-						d.onload = d.onreadystatechange = d.onerror = function(e) {
-							(e = e || this).type == "error" && b[i]
-								? l(b[i])
-								: e.type == "load" || e[g="readyState"] == "loaded" || e[g] == "complete"
-								? a[++i] ? l(a[i]) : r && r.apply(_, M)
-								: 0
-						},
+					// load CSS
+					(I = setInterval(function(q) {
+						if(d.sheet && d.sheet.cssRules && d.sheet.cssRules[0] || d[q="styleSheet"] && d[q].rules && d[q].rules[0]) {
+							try { a[++i] ? l(a[i]) : r && r.apply(_, M); } catch(e) {}
+							clearInterval(I)
+						}
+					}, 5)) && (d.rel = "stylesheet")
 
-						getElementsByTagName("head")[0].appendChild(d)
-				}(a[i=0]);
-			} catch(e) { C && C.log(e) }
+					:
+
+					// load JS
+					d.onload = d.onreadystatechange = d.onerror = function(e) {
+						(e = e || this).type == "error" && b[i]
+							? l(b[i])
+							: e.type == "load" || e[g="readyState"] == "loaded" || e[g] == "complete"
+							? a[++i] ? l(a[i]) : r && r.apply(_, M)
+							: 0
+					},
+
+					getElementsByTagName("head")[0].appendChild(d)
+			}(a[i=0]);
 
 			return _
 		})()
